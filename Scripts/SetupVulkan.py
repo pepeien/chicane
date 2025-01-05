12b7deaf5136da09707bb6b16ffba1374eb4533b
pepeien <erickfcaldeira@gmail.com>
2025-01-05 02:30:31 -0300

    Updated vulkan versio installed from the script

--- Scripts/SetupVulkan.py
+++ Scripts/SetupVulkan.py
@@ -10,7 +10,7 @@ from urllib.request import urlopen
 
 class VulkanConfiguration:
     requiredVulkanVersion = "1.3."
-    installVulkanVersion = "1.3.268.0"
+    installVulkanVersion = "1.3.269.0"
     vulkanDirectory = "./Vendor/vulkan"
 
     @classmethod
