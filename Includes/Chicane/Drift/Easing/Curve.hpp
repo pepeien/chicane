362810c32cb523713394eb8c92405fdefd0e5325
pepeien <58644295+pepeien@users.noreply.github.com>
2026-08-24 23:55:28 +0000

    style: Updated code format

--- Includes/Chicane/Drift/Easing/Curve.hpp
+++ Includes/Chicane/Drift/Easing/Curve.hpp
@@ -19,34 +19,34 @@ namespace Chicane
             static constexpr inline const char* TYPE_EASE_IN_OUT     = "ease-in-out";
             static constexpr inline const char* CUBIC_BEZIER_KEYWORD = "cubic-bezier";
 
-            static constexpr inline float LINEAR_X1 = 0.0f;
-            static constexpr inline float LINEAR_Y1 = 0.0f;
-            static constexpr inline float LINEAR_X2 = 1.0f;
-            static constexpr inline float LINEAR_Y2 = 1.0f;
+            static constexpr inline float       LINEAR_X1 = 0.0f;
+            static constexpr inline float       LINEAR_Y1 = 0.0f;
+            static constexpr inline float       LINEAR_X2 = 1.0f;
+            static constexpr inline float       LINEAR_Y2 = 1.0f;
 
-            static constexpr inline float EASE_X1 = 0.25f;
-            static constexpr inline float EASE_Y1 = 0.1f;
-            static constexpr inline float EASE_X2 = 0.25f;
-            static constexpr inline float EASE_Y2 = 1.0f;
+            static constexpr inline float       EASE_X1 = 0.25f;
+            static constexpr inline float       EASE_Y1 = 0.1f;
+            static constexpr inline float       EASE_X2 = 0.25f;
+            static constexpr inline float       EASE_Y2 = 1.0f;
 
-            static constexpr inline float EASE_IN_X1 = 0.42f;
-            static constexpr inline float EASE_IN_Y1 = 0.0f;
-            static constexpr inline float EASE_IN_X2 = 1.0f;
-            static constexpr inline float EASE_IN_Y2 = 1.0f;
+            static constexpr inline float       EASE_IN_X1 = 0.42f;
+            static constexpr inline float       EASE_IN_Y1 = 0.0f;
+            static constexpr inline float       EASE_IN_X2 = 1.0f;
+            static constexpr inline float       EASE_IN_Y2 = 1.0f;
 
-            static constexpr inline float EASE_OUT_X1 = 0.0f;
-            static constexpr inline float EASE_OUT_Y1 = 0.0f;
-            static constexpr inline float EASE_OUT_X2 = 0.58f;
-            static constexpr inline float EASE_OUT_Y2 = 1.0f;
+            static constexpr inline float       EASE_OUT_X1 = 0.0f;
+            static constexpr inline float       EASE_OUT_Y1 = 0.0f;
+            static constexpr inline float       EASE_OUT_X2 = 0.58f;
+            static constexpr inline float       EASE_OUT_Y2 = 1.0f;
 
-            static constexpr inline float EASE_IN_OUT_X1 = 0.42f;
-            static constexpr inline float EASE_IN_OUT_Y1 = 0.0f;
-            static constexpr inline float EASE_IN_OUT_X2 = 0.58f;
-            static constexpr inline float EASE_IN_OUT_Y2 = 1.0f;
+            static constexpr inline float       EASE_IN_OUT_X1 = 0.42f;
+            static constexpr inline float       EASE_IN_OUT_Y1 = 0.0f;
+            static constexpr inline float       EASE_IN_OUT_X2 = 0.58f;
+            static constexpr inline float       EASE_IN_OUT_Y2 = 1.0f;
 
-            static constexpr inline float SOLVE_EPSILON              = 1.0e-6f;
-            static constexpr inline int   SOLVE_NEWTON_ITERATIONS    = 8;
-            static constexpr inline int   SOLVE_BISECTION_ITERATIONS = 16;
+            static constexpr inline float       SOLVE_EPSILON              = 1.0e-6f;
+            static constexpr inline int         SOLVE_NEWTON_ITERATIONS    = 8;
+            static constexpr inline int         SOLVE_BISECTION_ITERATIONS = 16;
 
         public:
             static EasingCurve linear();
