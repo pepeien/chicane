d7425eb5f7951fcd9693a2b26f9b019aa4cef79e
pepeien <erickfcaldeira@gmail.com>
2026-09-12 02:38:50 -0300

    Added PI and radian related values to core math

---
+++ Includes/Chicane/Core/Math.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Chicane/Core.hpp"
+
+namespace Chicane
+{
+    static constexpr inline float PI         = 3.14159265358979323846f;
+    static constexpr inline float TWO_PI     = PI * 2.0f;
+    static constexpr inline float HALF_PI    = PI * 0.5f;
+    static constexpr inline float DEG_TO_RAD = PI / 180.0f;
+    static constexpr inline float RAD_TO_DEG = 180.0f / PI;
+}
